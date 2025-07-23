db2ba2cb27cd5beb1751a1dc39316662f9062719
dark-server <dark-server@Dark-Server.Dark-Server>
2025-07-23 17:57:06 +0100

    d

--- user/layouts/footer.php
+++ user/layouts/footer.php
@@ -56,22 +56,6 @@
     <script src="../assets/js/custom.js"></script>
     <script src="script.js"></script>
 
-    <!--Start of Tawk.to Script-->
-    <?php if(isset($livechat) && !empty($livechat)): ?>
-    <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='<?= $page['livechat'] ?>';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
-    <?php endif; ?>
-    <!--End of Tawk.to Script-->
-
     <?php if(function_exists('support_plugin')) support_plugin(); ?>
 </body>
 </html>
