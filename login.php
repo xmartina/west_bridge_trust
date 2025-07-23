b6ab80b84cf33cb88a3f3eb5b4204a55dee8e60d
dark-server <dark-server@Dark-Server.Dark-Server>
2025-07-23 18:00:58 +0100

    d

--- login.php
+++ login.php
@@ -108,6 +108,7 @@ if(isset($_POST['login'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - <?php echo WEB_TITLE; ?></title>
+    <link rel="icon" type="image/x-icon" href="/assets/images/logo/favicon.png" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
